1e29013ee910c5afef6b46b3d144ec8374efaa1b
Liam <43959073+LiamBird@users.noreply.github.com>
2024-09-13 16:24:37 +0100

    Update ParticleSizeDist.py
    Added 'scale' to input argument! Applied throughout, and original data is stored as self._data (from deepcopy) while scaled daa is output in the dataframe.
    Modified make_distribution to reflect LabelAnalysis functionality (previously limited)

--- ParticleSizeDist.py
+++ ParticleSizeDist.py
@@ -1,12 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import glob
-import pandas as pd
-import re
-
 class ParticleSizeDist(object):
-    def __init__(self, fname):
+    def __init__(self, fname, scale=1):
         '''
         Load and analyse particle size distribution data from ImageJ using XLib plugin
         'Features for printing' specified as: itvardsbg (with 'g' at end for bary centres)
@@ -51,36 +44,172 @@ class ParticleSizeDist(object):
                     
         data.update([("sphericity", 
                       np.pi**(1/3)*((6*np.array(data["volume"]))**(2/3))/np.array(data["surface"]))])
-        self.data = data
+        
+        from copy import deepcopy
+        self._data = deepcopy(data)
+        
+        for keys, values in data.items():
+            if keys in ["volume"]:
+                data[keys] = np.array(values)*scale**3
+                
+            if keys in ["surface"]:
+                data[keys] = np.array(values)*scale**2
+                
+            if keys in ["radius", "diameter", 'BaryX', 'BaryY', 'BaryZ']:
+                data[keys] = np.array(values)*scale
+        
         self._headers = header_line
         self.df = pd.DataFrame(data)
         
-    def make_volume_dist(self, nbins=20, min_radius=None, 
-                         max_radius=None, use_bins=None, 
-                         min_sphericity=0, max_sphericity=None):
-        if min_radius == None:
-            min_radius = np.min(self.df["radius"])
-        if max_radius == None:
-            max_radius = np.max(self.df["radius"])
-        
-        if type(use_bins) == type(None):
-            bins = np.linspace(min_radius, max_radius, nbins+1)
+    def make_distribution(self, distribution_variable="radius", sort_by_variable="volume", 
+                          use_bins=None, n_bins=20, percentile_min=0, percentile_max=100,
+                          dist_min=None, dist_max=None):
+        """
+        Makes a histogram-style distribution of the selected variable.
+        Bins the data into subgroups of selected variable and finds total volume of particles in each bin. 
+
+        Inputs
+        ----------
+        distribution_variable (str, default="EqDiameter"):
+            the variable to find the distribution of (x axis on histogram)
+
+        sort_by_variable (str, default="Volume3d"):
+            the variable over which to sum the groups of data (volume by default)
+
+        use_bins (defualt=None):
+            bin sizes from a different LabelAnalysis object, for matching x axes)
+
+        n_bins (int, default=20):
+            number of bins into which to sort the data (if use_bins==None)
+
+        percentile_min (float, default=0):
+            exclude the min x% of particles using the 'sort by' variable (e.g. by volume)
+
+        percentile_max (float, default=100):
+            exclude the max x% of particles using the 'sort by' variable (e.g. by volume)
+
+        dist_min (float, default=None):
+            minimum measurable size to include using the 'distribution' variable (e.g. equivalent diameter)
+
+        dist_max (float, default=None):
+            maximum measurable size to include using the 'distribution' variable (e.g. equivalent diameter)
+
+        mm_to_um (bool, default=True):
+            returns e.g. diameters, volumes in microns rather than mm
+
+        Returns
+        ----------
+        self.distribution:
+
+            Attributes
+            ----------
+            - x_bins: use for labelling - values of the x axis groups (e.g. in um)
+            - y_dist_abs: absolute values of the summed volumes (e.g. in um^3)
+            - y_dist: the percentage of the total volume in each bin (%)
+            - x_tick_pos: the x positions to use when plotting a histogram
+
+            Methods
+            ----------
+            plot_dist_bars(dist_self, xtickformatter="{:.1f}", percentage=True):
+                plots the distribution of the selected variable
+
+            Example
+            ----------
+            The x, y data for plotting without the automatic function can be accessed using:
+
+            ax.bar(self.distribution.x_tick_pos, self.distribution.y_dist)
+            ax.set_xticks(self.distribution.x_tick_pos)
+            ax.set_xticklabels(self.distribution.x_bins)
+
+        """
+        import pandas as pd
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        if all([type(limit)!=type(None) for limit in [dist_min, dist_max]]):
+            self.df = self.df.loc[(self.df[distribution_variable]>dist_min) & (self.df[distribution_variable]<dist_max)]
+        x_meas = np.array(self.df[sort_by_variable])
+        y_meas = np.array(self.df[distribution_variable])
+
+        sort_idx = np.argsort(x_meas)
+        sorted_x_all = x_meas[sort_idx]
+        sorted_y_all = y_meas[sort_idx]
+
+        lo_idx = int(sort_idx.shape[0]*percentile_min/100)
+        hi_idx = int(sort_idx.shape[0]*percentile_max/100)
+
+        sorted_x = sorted_x_all[lo_idx: hi_idx]
+        sorted_y = sorted_y_all[lo_idx: hi_idx]
+
+        if type(use_bins) ==  type(None):
+            y_min = np.min(sorted_y)
+            y_max = np.max(sorted_y)
+
+            bins = np.linspace(y_min, y_max, n_bins+1)
+
         else:
             bins = use_bins
-            nbins = bins.shape[0]-1
-        volume_dist = np.full((nbins, 3), np.nan)
-        
-        if type(max_sphericity) == type(None):
-            max_sphericity = 1e6
-            
-        for n in range(nbins):
-            volume_dist[n, 0] = bins[n]
-            volume_dist[n, 1] = bins[n+1]
-            volume_dist[n, 2] = np.sum(self.df.loc[(self.df["radius"]>=bins[n]) & 
-                                                   (self.df["radius"]<bins[n+1]) &
-                                                   (self.df["sphericity"]>min_sphericity) &
-                                                   (self.df["sphericity"]<max_sphericity)]["volume"])
-            
-        return pd.DataFrame({"min radius": volume_dist[:, 0],
-                "max_radius": volume_dist[:, 1],
-                "total volume": volume_dist[:, 2]})
+            n_bins = len(use_bins)-1
+
+        self.bins = bins
+
+        binned_idx = [np.argmin(abs(sorted_y-val)) for val in bins]
+        x_binned = [sorted_x[binned_idx[b]: binned_idx[b+1]] for b in range(n_bins)]
+        x_bin_sums = np.array([np.sum(x) for x in x_binned])
+
+        class _Dist(object):
+
+            def __init__(dist_self):
+                import pandas as pd
+                import numpy as np
+                import matplotlib.pyplot as plt
+                dist_self.x_bins = bins*scale
+                dist_self.y_dist_abs = np.array(x_bin_sums*scale**3)
+                dist_self.y_dist = dist_self.y_dist_abs/np.sum(dist_self.y_dist_abs)
+                dist_self.x_tick_pos = np.arange(n_bins)
+
+            def plot_dist_bars(dist_self, xtickformatter="{:.1f}", percentage=True):
+                import pandas as pd
+                import numpy as np
+                import matplotlib.pyplot as plt
+                """
+                Automatically plots the distribution as a bar chart (with axes formatted like histogram)
+
+                Inputs
+                ----------
+                xtickformatter (str, default={:.1f}):
+                    modifies the decimal places shown on the x axis tick labels
+
+                percentage (bool, default=True):
+                    if true, shows the bar heights as a percentage of the total
+                    if false, shows absolute units (e.g. um^3)
+
+                Returns
+                ----------
+                f, ax (can be used to modify axis, labels, etc)
+                """
+
+
+                f, ax = plt.subplots()
+                if percentage == True:
+                    ax.bar(self.distribution.x_tick_pos, self.distribution.y_dist)
+                else:
+                    ax.bar(self.distribution.x_tick_pos, self.distribution.y_dist_abs)
+
+                x_bins = self.distribution.x_bins
+                bin_spacing = x_bins[1]-x_bins[0]
+                bin_ticks = x_bins-bin_spacing
+                bin_tick_labels = []
+                for val in bin_ticks:
+                    if np.sign(val) == 1:
+                        bin_tick_labels.append(xtickformatter.format(val))
+                    else:
+                        bin_tick_labels.append(xtickformatter.format(0))
+
+                ax.set_xticks(list(self.distribution.x_tick_pos-0.5)+[self.distribution.x_tick_pos[-1]+0.5])
+
+                ax.set_xticklabels(bin_tick_labels)
+                dist_self._bin_tick_labels = bin_tick_labels
+                return f, ax
+
+        self.distribution = _Dist()
